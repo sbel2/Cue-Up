802b3e11607b17e1f1e7bc3705ced2845a40da10
Chi <richardmlfns@gmail.com>
2024-07-25 16:50:56 +0800

    Working Search Function based on Title Keywords - Change File Names

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useState } from 'react';
-import SearchBar from '@/components/SearchBar';
+import SearchBar from '@/components/searchBar';
 import { createClient } from '@supabase/supabase-js';
 
 // Initialize Supabase client
