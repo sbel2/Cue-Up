beac9553c52b7c1e378c0aef8cd22adf3b688346
Chi <richardmlfns@gmail.com>
2024-07-25 16:45:19 +0800

    Working Search Function based on Title Keywords Final

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -26,36 +26,3 @@ const SearchBar = ({ onSearch }: { onSearch: (query: string) => void }) => {
 };
 
 export default SearchBar;
-
-
-// "use client";
-
-// import React, { useState } from 'react';
-
-// interface SearchBarProps {
-//   onSearch: (query: string) => void;
-// }
-
-// const SearchBar: React.FC<SearchBarProps> = ({ onSearch }) => {
-//   const [query, setQuery] = useState('');
-
-//   const handleSearch = (e: React.FormEvent) => {
-//     e.preventDefault();
-//     onSearch(query);
-//   };
-
-//   return (
-//     <form onSubmit={handleSearch} className="search-bar">
-//       <input
-//         type="text"
-//         value={query}
-//         onChange={(e) => setQuery(e.target.value)}
-//         placeholder="Search posts..."
-//         className="search-input"
-//       />
-//       <button type="submit" className="search-button">Search</button>
-//     </form>
-//   );
-// };
-
-// export default SearchBar;
